3f75cd84b292b6c8c8cd04a7b311f1a3cfd1867b
scomor55 <scomor2@etf.unsa.ba>
2022-12-08 00:49:19 +0100

    initial commit

---
+++ flightReservation/src/main/java/ba/unsa/etf/rpr/dao/TicketsDaoSQLImpl.java
@@ -0,0 +1,40 @@
+package ba.unsa.etf.rpr.dao;
+
+import ba.unsa.etf.rpr.domain.Flights;
+import ba.unsa.etf.rpr.domain.Tickets;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TicketsDaoSQLImpl implements TicketsDao{
+    @Override
+    public Tickets getById(int id) {
+        return null;
+    }
+
+    @Override
+    public Tickets add(Tickets item) {
+        return null;
+    }
+
+    @Override
+    public Tickets update(Tickets item) {
+        return null;
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+
+    @Override
+    public List<Tickets> getAll() {
+        return null;
+    }
+
+    @Override
+    public List<Tickets> searchByClass(String ticketClass) {
+        return null;
+    }
+}

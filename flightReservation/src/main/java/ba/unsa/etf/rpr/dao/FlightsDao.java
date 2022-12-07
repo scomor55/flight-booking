732a5a7164dbecfa0d317772ebbc874a63be3a13
scomor55 <scomor2@etf.unsa.ba>
2022-12-07 01:05:04 +0100

    initial commit

---
+++ flightReservation/src/main/java/ba/unsa/etf/rpr/dao/FlightsDao.java
@@ -0,0 +1,11 @@
+package ba.unsa.etf.rpr.dao;
+
+import ba.unsa.etf.rpr.domain.Flights;
+
+/**
+ * Dao interface for Passengers domain bean
+ *
+ * @author Safet Čomor
+ */
+public interface FlightsDao extends Dao<Flights> {
+}

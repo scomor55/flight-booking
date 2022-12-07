24eca5152cce0478384694b51c72752632a58529
scomor55 <scomor2@etf.unsa.ba>
2022-12-07 01:09:05 +0100

    initial commit

---
+++ flightReservation/src/main/java/ba/unsa/etf/rpr/dao/TicketsDao.java
@@ -0,0 +1,12 @@
+package ba.unsa.etf.rpr.dao;
+
+import ba.unsa.etf.rpr.domain.Tickets;
+
+/**
+ * Dao interface for Tickets domain bean
+ *
+ * @author Safet Čomor
+ */
+
+public interface TicketsDao extends Dao<Tickets>{
+}
--- flightReservation/src/main/java/ba/unsa/etf/rpr/domain/Tickets.java
+++ flightReservation/src/main/java/ba/unsa/etf/rpr/domain/Tickets.java
@@ -1,4 +1,4 @@
 package ba.unsa.etf.rpr.domain;
 
-public class Ticket {
+public class Tickets {
 }

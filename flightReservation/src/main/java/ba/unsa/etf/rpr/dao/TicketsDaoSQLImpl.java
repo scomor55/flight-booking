8f06ce42dbd082b31b891012cc88732ba8291410
scomor55 <scomor2@etf.unsa.ba>
2022-12-08 00:56:42 +0100

    getById implementation

--- flightReservation/src/main/java/ba/unsa/etf/rpr/dao/TicketsDaoSQLImpl.java
+++ flightReservation/src/main/java/ba/unsa/etf/rpr/dao/TicketsDaoSQLImpl.java
@@ -8,8 +8,39 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TicketsDaoSQLImpl implements TicketsDao{
+
+    private Connection connection;
+
+    public TicketsDaoSQLImpl(){
+        try {
+            this.connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/root", "root", "root");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     @Override
     public Tickets getById(int id) {
+        String query = "SELECT * FROM Tickets WHERE ticketID = ?";
+        try {
+            PreparedStatement stmt = this.connection.prepareStatement(query);
+            stmt.setInt(1, id);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) { // result set is iterator.
+                Tickets ticket = new Tickets();
+                ticket.setTicketID(rs.getInt("ticketID"));
+                ticket.setFlightID(rs.getInt("flightID"));
+                ticket.setPassengerID(rs.getInt("passengerID"));
+                ticket.setTravelClass(rs.getString("class"));
+                ticket.setPrice(rs.getInt("price"));
+                rs.close();
+                return ticket;
+            } else {
+                return null; // if there is no elements in the result set return null
+            }
+        } catch (SQLException e) {
+            e.printStackTrace(); // poor error handling
+        }
         return null;
     }
 

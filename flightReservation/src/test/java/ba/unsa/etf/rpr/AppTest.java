8eada1703930af444d9d56eafe884bedfbc1d663
scomor55 <scomor2@etf.unsa.ba>
2022-12-09 01:43:17 +0100

    latest update

--- flightReservation/src/test/java/ba/unsa/etf/rpr/AppTest.java
+++ flightReservation/src/test/java/ba/unsa/etf/rpr/AppTest.java
@@ -1,8 +1,10 @@
 package ba.unsa.etf.rpr;
 
-import static org.junit.Assert.assertTrue;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 /**
  * Unit test for simple App.

0a8dfc8f4f4c3c428f8dd45c7b8dbbc01e8924e9
scomor55 <scomor2@etf.unsa.ba>
2022-12-15 00:42:33 +0100

    latest update

---
+++ flightReservation/src/main/java/ba/unsa/etf/rpr/Main.java
@@ -0,0 +1,33 @@
+package ba.unsa.etf.rpr;
+
+import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+            Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+            stage.setTitle("Hello World");
+            stage.setScene(new Scene(root,300,275));
+            stage.show();
+    }
+}

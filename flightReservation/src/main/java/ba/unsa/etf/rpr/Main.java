42fcc103ef764cdd03bd519af2dd30678988126c
scomor55 <scomor2@etf.unsa.ba>
2022-12-15 00:59:21 +0100

    latest update

--- flightReservation/src/main/java/ba/unsa/etf/rpr/Main.java
+++ flightReservation/src/main/java/ba/unsa/etf/rpr/Main.java
@@ -23,11 +23,42 @@ import java.io.IOException;
 
 public class Main extends Application {
 
-    @Override
-    public void start(Stage stage) throws Exception {
-            Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-            stage.setTitle("Hello World");
-            stage.setScene(new Scene(root,300,275));
-            stage.show();
+
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+        launch(args);
     }
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // FXML style
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Dummy Login App");
+        primaryStage.setScene(new Scene(root, 300, 110));
+        primaryStage.setResizable(false);
+        primaryStage.show();
+
+        /*
+        // Class style
+        Label userIdLbl = new Label("Username:");
+        TextField userIdTxt = new TextField();
+        Label userPwdLbl = new Label("Password:");
+        PasswordField userPwdTxt = new PasswordField();
+        GridPane root = new GridPane();
+        root.setVgap(20);
+        //root.setPadding(new Insets(10));
+        root.setAlignment(Pos.CENTER);
+        GridPane.setConstraints(userIdLbl, 0, 0);
+        GridPane.setConstraints(userIdTxt, 1, 0);
+        GridPane.setConstraints(userPwdLbl, 0, 1);
+        GridPane.setConstraints(userPwdTxt, 1, 1);
+        root.getChildren().addAll(userIdLbl, userIdTxt, userPwdLbl, userPwdTxt);
+        Button signInBtn = new Button ("Login");
+        signInBtn.setPrefWidth(Double.MAX_VALUE);
+        root.add(signInBtn,0,2,2,1);
+        Scene scene = new Scene(root,250,150);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        */
+    }
+
 }

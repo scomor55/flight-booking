ea02b4d5d97f1c0307e75afe9e569a64e34c629a
scomor55 <scomor2@etf.unsa.ba>
2022-12-15 22:13:19 +0100

    refactoring of dao

--- flightReservation/src/main/java/ba/unsa/etf/rpr/dao/Dao.java
+++ flightReservation/src/main/java/ba/unsa/etf/rpr/dao/Dao.java
@@ -1,5 +1,7 @@
 package ba.unsa.etf.rpr.dao;
 
+import ba.unsa.etf.rpr.exceptions.FlightBookingException;
+
 import java.util.List;
 
 /**
@@ -13,31 +15,31 @@ public interface Dao<T> {
      * @param id primary key of entity
      * @return Entity from database
      */
-    T getById(int id);
+    T getById(int id) throws FlightBookingException;
 
     /**
      * Saves entity into database
      * @param item bean for saving to database
      * @return saved item with id field populated
      */
-    T add(T item);
+    T add(T item) throws FlightBookingException;
 
     /**
      * Fully updates entity in database based on id (primary) match.
      * @param item - bean to be updated. id must be populated
      * @return updated version of bean
      */
-    T update(T item);
+    T update(T item)  throws FlightBookingException;
 
     /**
      * Hard delete of item from database with given id
      * @param id - primary key of entity
      */
-    void delete(int id);
+    void delete(int id)  throws FlightBookingException;
 
     /**
      * Lists all entities from database. WARNING: Very slow operation because it reads all records.
      * @return List of entities from database
      */
-    List<T> getAll();
+    List<T> getAll()  throws FlightBookingException;
 }

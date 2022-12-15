30ca97c568a4bec2d939c681303b1d81d5e1a187
scomor55 <scomor2@etf.unsa.ba>
2022-12-15 23:14:51 +0100

    refactoring of dao

---
+++ flightReservation/src/main/java/ba/unsa/etf/rpr/dao/DaoFactory.java
@@ -0,0 +1,32 @@
+package ba.unsa.etf.rpr.dao;
+
+
+/**
+ * Factory method for singleton implementation of DAOs
+ *
+ * @author Safet Čomor
+ */
+
+public class DaoFactory {
+
+    private static final FlightsDao flightsDao = new FlightsDaoSQLImpl();
+
+    private static final PassengersDao passengersDao = new PassengersDaoSQLImpl();
+    private static final TicketsDao ticketsDao = new TicketsDaoSQLImpl();
+
+    private DaoFactory(){
+    }
+
+    public static FlightsDao flightsDao(){
+        return flightsDao;
+    }
+
+    public static PassengersDao passengersDao(){
+        return passengersDao;
+    }
+
+    public static TicketsDao ticketsDao(){
+        return ticketsDao;
+    }
+
+}

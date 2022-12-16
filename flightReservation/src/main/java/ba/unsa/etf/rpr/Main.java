4788968bde24e6b7da6ea13847e3482fb4facef6
scomor55 <scomor2@etf.unsa.ba>
2022-12-16 23:23:08 +0100

    latest update

--- flightReservation/src/main/java/ba/unsa/etf/rpr/Main.java
+++ flightReservation/src/main/java/ba/unsa/etf/rpr/Main.java
@@ -12,12 +12,14 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
+
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/sample.fxml"));
-        primaryStage.setTitle("Login");
-        primaryStage.setScene(new Scene(root, 300, 125));
+        primaryStage.setTitle("Prijava");
+        primaryStage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
         primaryStage.setResizable(false);
         primaryStage.show();
     }

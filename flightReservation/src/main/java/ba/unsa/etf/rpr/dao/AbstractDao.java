036f092e085f2ee7a2dd93a77be1c198eb918257
scomor55 <scomor2@etf.unsa.ba>
2022-12-18 21:57:13 +0100

    latest update

--- flightReservation/src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
+++ flightReservation/src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
@@ -16,7 +16,10 @@ public abstract class AbstractDao <T extends Idable> implements Dao<T> {
             this.tableName = tableName;
             Properties p = new Properties();
             p.load(ClassLoader.getSystemResource("application.properties.template").openStream());
-            this.connection = DriverManager.getConnection(p.getProperty("db.connection_string"), p.getProperty("db.username"), p.getProperty("db.password"));
+            String url = p.getProperty("db.connection_string");
+            String username = p.getProperty("db.username");
+            String password = p.getProperty("db.password");
+            this.connection = DriverManager.getConnection(url,username,password);
         } catch (Exception e) {
             e.printStackTrace();
         }

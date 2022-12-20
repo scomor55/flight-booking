7aa5189610ccb4c378feb970ac28c8baf6c6e05f
scomor55 <scomor2@etf.unsa.ba>
2022-12-20 19:22:50 +0100

    initial commit

---
+++ flightReservation/src/main/java/ba/unsa/etf/rpr/controllers/Register.java
@@ -0,0 +1,26 @@
+package ba.unsa.etf.rpr.controllers;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
+
+public class Register extends Application {
+
+    public static void main(String[] args) {
+        // System.out.println("Hello World!");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/login.fxml"));
+        stage.setTitle("Login App");
+        stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        stage.setResizable(false);
+        stage.show();
+    }
+}

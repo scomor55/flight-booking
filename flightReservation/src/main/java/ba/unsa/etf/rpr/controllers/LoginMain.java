8d58a38617b2fafaa3c5845076539e468bbdb7aa
scomor55 <scomor2@etf.unsa.ba>
2022-12-20 01:04:07 +0100

    latest update

--- flightReservation/src/main/java/ba/unsa/etf/rpr/controllers/LoginMain.java
+++ flightReservation/src/main/java/ba/unsa/etf/rpr/controllers/LoginMain.java
@@ -7,8 +7,19 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class LoginMain extends Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        launch(args);
+    }
+
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/login.fxml"));
+        primaryStage.setTitle("Login App");
+        primaryStage.setScene(new Scene(root, 300, 110));
+        primaryStage.setResizable(false);
+        primaryStage.show();
 
     }
 }

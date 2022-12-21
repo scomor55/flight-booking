bbee57b83fc6f78569ee4e438101629ee2d830d9
scomor55 <scomor2@etf.unsa.ba>
2022-12-22 00:57:54 +0100

    implemented start method

---
+++ flightReservation/src/main/java/ba/unsa/etf/rpr/controllers/AdminFlightsController.java
@@ -0,0 +1,26 @@
+package ba.unsa.etf.rpr.controllers;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
+
+public class AdminFlightsController extends Application {
+
+    public static void main(String[] args) {
+        // System.out.println("Hello World!");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/adminFlights.fxml"));
+        stage.setTitle("Flights management");
+        stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        stage.setResizable(false);
+        stage.show();
+    }
+}

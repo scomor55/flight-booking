4549f353883f319b4026c331faa2b532505d992b
scomor55 <scomor2@etf.unsa.ba>
2022-12-22 00:27:11 +0100

    initial commit

---
+++ flightReservation/src/main/java/ba/unsa/etf/rpr/controllers/AdminPanelController.java
@@ -0,0 +1,17 @@
+package ba.unsa.etf.rpr.controllers;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class AdminPanelController extends Application {
+
+    public static void main(String[] args) {
+        // System.out.println("Hello World!");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}

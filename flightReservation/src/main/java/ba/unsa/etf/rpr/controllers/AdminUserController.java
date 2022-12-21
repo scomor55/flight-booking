ef9b2f05beb2e51c044842a6aa824fb4c25048ff
scomor55 <scomor2@etf.unsa.ba>
2022-12-22 00:32:38 +0100

    initial commit

---
+++ flightReservation/src/main/java/ba/unsa/etf/rpr/controllers/AdminUserController.java
@@ -0,0 +1,22 @@
+package ba.unsa.etf.rpr.controllers;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
+
+public class AdminUserController extends Application {
+
+    public static void main(String[] args) {
+        // System.out.println("Hello World!");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}

a871bde0c0d06408c121805e9143c80c0461a207
scomor55 <scomor2@etf.unsa.ba>
2022-12-22 00:29:10 +0100

    implemented start method

--- flightReservation/src/main/java/ba/unsa/etf/rpr/controllers/AdminPanelController.java
+++ flightReservation/src/main/java/ba/unsa/etf/rpr/controllers/AdminPanelController.java
@@ -1,7 +1,12 @@
 package ba.unsa.etf.rpr.controllers;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
 public class AdminPanelController extends Application {
 
@@ -12,6 +17,10 @@ public class AdminPanelController extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/adminPanel.fxml"));
+        stage.setTitle("Login App");
+        stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        stage.setResizable(false);
+        stage.show();
     }
 }

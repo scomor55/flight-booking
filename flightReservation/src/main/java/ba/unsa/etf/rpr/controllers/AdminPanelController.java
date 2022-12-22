118e7523be29f7f3256130980ba2fd55273d9296
scomor55 <scomor2@etf.unsa.ba>
2022-12-22 21:02:23 +0100

    more updates

--- flightReservation/src/main/java/ba/unsa/etf/rpr/controllers/AdminPanelController.java
+++ flightReservation/src/main/java/ba/unsa/etf/rpr/controllers/AdminPanelController.java
@@ -1,10 +1,13 @@
 package ba.unsa.etf.rpr.controllers;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
@@ -21,5 +24,55 @@ public class AdminPanelController extends Application {
         stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
         stage.setResizable(false);
         stage.show();
+    }
+
+    public void userClick(ActionEvent actionEvent) throws IOException {
+        Stage userStage = new Stage();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/adminUser.fxml"));
+        Parent root = loader.load();
+       /* AdminUsersController adminUsersController = new AdminUsersController();
+        loader.setController(adminUsersController);*/
+        userStage.setTitle("User management");
+        userStage.setScene(new Scene(root,USE_COMPUTED_SIZE,USE_COMPUTED_SIZE));
+        userStage.setResizable(false);
+        userStage.show();
+    }
+
+    public void passengerClick(ActionEvent actionEvent)throws IOException {
+        Stage passengerStage = new Stage();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/adminPassengers.fxml"));
+        Parent root = loader.load();
+       /* AdminUsersController adminUsersController = new AdminUsersController();
+        loader.setController(adminUsersController);*/
+        passengerStage.setTitle("Passengers management");
+        passengerStage.setScene(new Scene(root,USE_COMPUTED_SIZE,USE_COMPUTED_SIZE));
+        passengerStage.setResizable(false);
+        passengerStage.show();
+    }
+
+
+    public void flightsClick(ActionEvent actionEvent)throws IOException {
+        Stage flightsStage = new Stage();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/adminFlights.fxml"));
+        Parent root = loader.load();
+       /* AdminUsersController adminUsersController = new AdminUsersController();
+        loader.setController(adminUsersController);*/
+        flightsStage.setTitle("Flights management");
+        flightsStage.setScene(new Scene(root,USE_COMPUTED_SIZE,USE_COMPUTED_SIZE));
+        flightsStage.setResizable(false);
+        flightsStage.show();
+    }
+
+
+    public void ticketsClick(ActionEvent actionEvent)throws IOException{
+        Stage ticketsStage = new Stage();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/adminTickets.fxml"));
+        Parent root = loader.load();
+       /* AdminUsersController adminUsersController = new AdminUsersController();
+        loader.setController(adminUsersController);*/
+        ticketsStage.setTitle("Tickets management");
+        ticketsStage.setScene(new Scene(root,USE_COMPUTED_SIZE,USE_COMPUTED_SIZE));
+        ticketsStage.setResizable(false);
+        ticketsStage.show();
     }
 }

f7c11dffb9d04364fb6f7bdc7be4d4fb311bf2ee
scomor55 <scomor2@etf.unsa.ba>
2022-12-22 22:52:47 +0100

    bug fix

--- flightReservation/src/main/java/ba/unsa/etf/rpr/controllers/Controller.java
+++ flightReservation/src/main/java/ba/unsa/etf/rpr/controllers/Controller.java
@@ -100,17 +100,16 @@ public class Controller {
             if(tempUsername.equals("safa")){
 
                 try {
-                final Stage adminStage = new Stage();
-                FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/adminPanel.fxml"));
-                loader.load();
-                Register register = loader.getController();
-                adminStage.setTitle("Admin panel");
-                adminStage.setScene(new Scene(loader.getRoot(),USE_COMPUTED_SIZE,USE_COMPUTED_SIZE));
-                adminStage.setResizable(false);
-                adminStage.show();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
+                    final Stage adminStage = new Stage();
+                    FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/adminPanel.fxml"));
+                    loader.load();
+                    adminStage.setTitle("Admin panel");
+                    adminStage.setScene(new Scene(loader.getRoot(),USE_COMPUTED_SIZE,USE_COMPUTED_SIZE));
+                    adminStage.setResizable(false);
+                    adminStage.show();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
             }
         }
     }

07ab8457d063d1b8fbe0831dff89e2e1391f915a
scomor55 <scomor2@etf.unsa.ba>
2022-12-23 03:00:31 +0100

    initial commit

---
+++ flightReservation/src/main/java/ba/unsa/etf/rpr/business/PassengersManager.java
@@ -0,0 +1,9 @@
+package ba.unsa.etf.rpr.business;
+
+import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.domain.Passengers;
+import ba.unsa.etf.rpr.exceptions.FlightBookingException;
+import java.util.List;
+
+public class PassengersManager {
+}

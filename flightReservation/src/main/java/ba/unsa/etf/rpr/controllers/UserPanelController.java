aa82291892efc93ea564f841a1257e5a16895ce0
scomor55 <scomor2@etf.unsa.ba>
2022-12-24 00:10:09 +0100

    removing unnecessary code

--- flightReservation/src/main/java/ba/unsa/etf/rpr/controllers/UserPanelController.java
+++ flightReservation/src/main/java/ba/unsa/etf/rpr/controllers/UserPanelController.java
@@ -8,19 +8,7 @@ import javafx.stage.Stage;
 
 import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
-public class UserPanelController extends Application {
+public class UserPanelController  {
 
-    public static void main(String[] args) {
 
-        launch(args);
-    }
-
-    @Override
-    public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/userPanel.fxml"));
-        stage.setTitle("User view");
-        stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
-        stage.setResizable(false);
-        stage.show();
-    }
 }

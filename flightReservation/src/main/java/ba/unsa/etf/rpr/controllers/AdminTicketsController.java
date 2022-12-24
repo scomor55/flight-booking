44fad2389571d8f4e928f749a10d7205341000e6
scomor55 <scomor2@etf.unsa.ba>
2022-12-24 03:44:28 +0100

    latest update

--- flightReservation/src/main/java/ba/unsa/etf/rpr/controllers/AdminTicketsController.java
+++ flightReservation/src/main/java/ba/unsa/etf/rpr/controllers/AdminTicketsController.java
@@ -7,12 +7,15 @@ import ba.unsa.etf.rpr.domain.Passengers;
 import ba.unsa.etf.rpr.domain.Tickets;
 import ba.unsa.etf.rpr.exceptions.FlightBookingException;
 import javafx.application.Application;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
@@ -25,7 +28,28 @@ public class AdminTicketsController  {
     public TextField classField;
     public TextField priceField;
     public TextField ticketIDField;
+    public ListView<Tickets> ticketsList;
+
     private TicketsManager manager = new TicketsManager();
+
+    @FXML
+    public void initialize(){
+        try {
+            refreshTickets();
+            ticketsList.getSelectionModel().selectedItemProperty().addListener((obs, o , n) ->{
+                if(n != null){
+                   flightIdField.setText(String.valueOf(n.getFlightID()));
+                   passengerIdField.setText(String.valueOf(n.getPassengerID()));
+                   classField.setText(n.getTravelClass());
+                   priceField.setText(String.valueOf(n.getPrice()));
+                   ticketIDField.setText(String.valueOf(n.getId()));
+                }
+            });
+        }catch(FlightBookingException f){
+            throw new RuntimeException(f);
+        }
+    }
+
     public void addTicket(ActionEvent actionEvent) {
         try {
             Tickets ticket = new Tickets();
@@ -52,4 +76,15 @@ public class AdminTicketsController  {
         Tickets ticket = manager.getById(Integer.parseInt(ticketIDField.getText()));
         manager.delete(ticket.getId());
     }
+
+    private void refreshTickets()throws FlightBookingException{
+        try{
+            ticketsList.setItems(FXCollections.observableList(manager.getAll()));
+        }catch(FlightBookingException f){
+            new Alert(Alert.AlertType.NONE, f.getMessage(), ButtonType.OK).show();
+        }
+    }
 }
+
+
+

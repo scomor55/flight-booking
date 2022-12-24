4c50dd3685583269a745b127bcd02f23633c9907
scomor55 <scomor2@etf.unsa.ba>
2022-12-24 03:35:58 +0100

    removing unnecessary code

--- flightReservation/src/main/java/ba/unsa/etf/rpr/controllers/AdminUsersController.java
+++ flightReservation/src/main/java/ba/unsa/etf/rpr/controllers/AdminUsersController.java
@@ -9,20 +9,11 @@ import javafx.stage.Stage;
 
 import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
-public class AdminUsersController extends Application {
+public class AdminUsersController  {
 
-    public static void main(String[] args) {
-        launch(args);
-    }
 
-    @Override
-    public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/adminUser.fxml"));
-        stage.setTitle("User management");
-        stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
-        stage.setResizable(false);
-        stage.show();
-    }
+
+
 /* Implementation of functionalities starts here */
     public void addUser(ActionEvent actionEvent) {
 

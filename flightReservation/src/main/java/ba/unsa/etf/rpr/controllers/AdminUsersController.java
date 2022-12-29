cfc64d88d6145b7a08627c21264f476b57398be9
scomor55 <scomor2@etf.unsa.ba>
2022-12-30 00:00:49 +0100

    latest update

--- flightReservation/src/main/java/ba/unsa/etf/rpr/controllers/AdminUsersController.java
+++ flightReservation/src/main/java/ba/unsa/etf/rpr/controllers/AdminUsersController.java
@@ -5,6 +5,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
@@ -12,9 +13,11 @@ import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 public class AdminUsersController  {
 
 
+    public TextField usernameField;
+    public TextField passwordField;
+    public TextField idField;
 
-
-/* Implementation of functionalities starts here */
+    /* Implementation of functionalities starts here */
     public void addUser(ActionEvent actionEvent) {
 
     }

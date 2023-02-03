37d7dd686c9d4e0f0ba9446f53ef970f66fd73ca
scomor55 <scomor2@etf.unsa.ba>
2023-02-03 23:23:52 +0100

    javadoc added

--- flightReservation/src/main/java/ba/unsa/etf/rpr/dao/FlightsDao.java
+++ flightReservation/src/main/java/ba/unsa/etf/rpr/dao/FlightsDao.java
@@ -11,10 +11,34 @@ import java.util.List;
  */
 public interface FlightsDao extends Dao<Flights> {
 
+    /**
+     * Search for flights based on their source location.
+     * @param flightSource the source location of the flight.
+     * @return a list of flights with the specified source location.
+     * @throws FlightBookingException if an error occurs while searching for flights.
+*/
     List<Flights> searchBySource(String flightSource) throws FlightBookingException ;
+    /**
+     * Search for flights based on their destination location.
+     * @param flightDestination the destination location of the flight.
+     * @return a list of flights with the specified destination location.
+     * @throws FlightBookingException if an error occurs while searching for flights.
+     */
     List<Flights> searchByDestination(String flightDestination) throws FlightBookingException ;
+    /**
+     * Search for flights based on both their source and destination location.
+     * @param flightSource the source location of the flight.
+     * @param flightDestination the destination location of the flight.
+     * @return a list of flights with the specified source and destination location.
+     * @throws FlightBookingException if an error occurs while searching for flights.
+     */
     List<Flights> searchBySourceAndDestination(String flightSource,String flightDestination) throws FlightBookingException ;
-
+    /**
+     * Search for flights for a specific passenger.
+     * @param passengerID the ID of the passenger.
+     * @return a list of flights for the specified passenger.
+     * @throws FlightBookingException if an error occurs while searching for flights.
+     */
     public List<Flights> searchFlightsForPassenger(int passengerID ) throws FlightBookingException;
 
 }

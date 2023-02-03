123a6f58cf201465b3c13e71f4055cf1afe68bbc
scomor55 <scomor2@etf.unsa.ba>
2023-02-03 23:02:03 +0100

    javadoc added

--- flightReservation/src/main/java/ba/unsa/etf/rpr/dao/DaoFactory.java
+++ flightReservation/src/main/java/ba/unsa/etf/rpr/dao/DaoFactory.java
@@ -8,23 +8,41 @@ import java.sql.Connection;
  */
 
 public class DaoFactory {
-
+    /**
+     * Instance of FlightsDao implementation
+     * **/
     private static final FlightsDao flightsDao = FlightsDaoSQLImpl.getInstance();
-
+    /**
+     *  Instance of PassengersDao implementation
+     **/
     private static final PassengersDao passengersDao = PassengersDaoSQLImpl.getInstance();
+    /**
+     *  Instance of TicketsDao implementation
+     **/
     private static final TicketsDao ticketsDao = TicketsDaoSQLImpl.getInstance();
-
+    /**
+     * Private constructor to prevent creating instances of this class
+     */
     private DaoFactory(){
     }
-
+    /**
+     * Returns an instance of FlightsDao implementation.
+     * @return an instance of FlightsDao implementation.
+     */
     public static FlightsDao flightsDao(){
         return flightsDao;
     }
-
+    /**
+     * Returns an instance of PassengersDao implementation.
+     * @return an instance of PassengersDao implementation.
+     */
     public static PassengersDao passengersDao(){
         return passengersDao;
     }
-
+    /**
+     * Returns an instance of TicketsDao implementation.
+     * @return an instance of TicketsDao implementation.
+     */
     public static TicketsDao ticketsDao(){
         return ticketsDao;
     }

5742376c7de365c3aaa906c562981908f97dfe1d
scomor55 <scomor2@etf.unsa.ba>
2023-02-03 23:39:13 +0100

    javadoc added

--- flightReservation/src/main/java/ba/unsa/etf/rpr/dao/PassengersDaoSQLImpl.java
+++ flightReservation/src/main/java/ba/unsa/etf/rpr/dao/PassengersDaoSQLImpl.java
@@ -13,21 +13,34 @@ import java.util.TreeMap;
 public class PassengersDaoSQLImpl extends AbstractDao<Passengers> implements PassengersDao {
 
     private static PassengersDaoSQLImpl instance = null;
+    /**
+     * Private constructor that calls the super class constructor with the table name "Passengers".
+     */
     private PassengersDaoSQLImpl() {
         super("Passengers");
     }
-
+    /**
+     * Returns the single instance of this class. If no instance exists, creates one.
+     * @return The single instance of this class
+     */
     public static PassengersDaoSQLImpl getInstance(){
         if(instance==null)
             instance = new PassengersDaoSQLImpl();
         return instance;
     }
-
+    /**
+     * Removes the current instance of this class.
+     */
     public static void removeInstance(){
         if(instance!=null)
             instance=null;
     }
-
+    /**
+     * Converts a row from the "Passengers" table in the database to a Passengers object.
+     * @param rs The ResultSet representing a row from the "Passengers" table.
+     * @return A Passengers object created from the data in the ResultSet.
+     * @throws FlightBookingException If there was an error reading from the ResultSet.
+     */
     @Override
     public Passengers row2object(ResultSet rs) throws FlightBookingException{
         try {
@@ -43,7 +56,11 @@ public class PassengersDaoSQLImpl extends AbstractDao<Passengers> implements Pas
             throw new FlightBookingException(e.getMessage(), e);
         }
     }
-
+    /**
+     * Converts a Passengers object to a map representing a row in the "Passengers" table.
+     * @param object The Passengers object to be converted.
+     * @return A Map representing a row in the "Passengers" table, with keys as column names and values as column values.
+     */
     @Override
     public Map<String, Object> object2row(Passengers object) {
         Map<String, Object> item = new TreeMap<>();
@@ -55,12 +72,22 @@ public class PassengersDaoSQLImpl extends AbstractDao<Passengers> implements Pas
         item.put("email",object.getEmail());
         return item;
     }
-
+    /**
+     * This method is used to search the passengers by their name.
+     * @param name Name of the passenger to be searched.
+     * @return A List of {@link Passengers} objects that match the name provided as a parameter.
+     * @throws FlightBookingException when an exception occurs during the execution of this method.
+     */
     @Override
     public List<Passengers> searchByName(String name)throws FlightBookingException{
         return executeQuery("SELECT * FROM Passengers WHERE name LIKE concat('%', ?, '%')",new Object[]{name});
     }
-
+    /**
+     * This method is used to search the passengers by their surname.
+     * @param surname Surname of the passenger to be searched.
+     * @return A List of {@link Passengers} objects that match the surname provided as a parameter.
+     * @throws FlightBookingException when an exception occurs during the execution of this method.
+     */
     @Override
     public List<Passengers> searchBySurname(String surname) throws FlightBookingException{
 

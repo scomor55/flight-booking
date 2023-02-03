ee2b9377bcf71a47d501a88fd8a9154ddf8c2e7a
scomor55 <scomor2@etf.unsa.ba>
2023-02-03 22:21:25 +0100

    javadoc added

--- flightReservation/src/main/java/ba/unsa/etf/rpr/exceptions/FlightBookingException.java
+++ flightReservation/src/main/java/ba/unsa/etf/rpr/exceptions/FlightBookingException.java
@@ -1,11 +1,27 @@
 package ba.unsa.etf.rpr.exceptions;
 
+/**
+ * The FlightBookingException class is a custom exception that extends the standard Java Exception class
+ * It is used to handle exceptions that occur during flight booking.
+ * @author Safet Čomor
+ * @version 1.0
+ *
+ */
+
 public class FlightBookingException extends Exception{
 
+    /**
+     * Constructs a new FlightBookingException with the specified message and reason.
+     * @param msg the detail message.
+     * @param reason the cause of the exception.
+     */
     public FlightBookingException(String msg, Exception reason){
         super(msg, reason);
     }
-
+    /**
+     * Constructs a new FlightBookingException with the specified message.
+     * @param msg the detail message.
+     */
     public FlightBookingException(String msg){
         super(msg);
     }
